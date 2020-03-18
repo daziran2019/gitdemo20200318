46f71d6e44d725dffb96e15657c0202d27e729cd
daziran <343052690@qq.com>
2020-03-18 13:23:46 +0800

    mapper to git

---
+++ src/main/java/com/atguigu/gitdemo20200318/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.atguigu.gitdemo20200318.mapper;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserMapper {
+    public static void main(String[] args) {
+        System.out.println("This is mapper ... ");
+    }
+}
