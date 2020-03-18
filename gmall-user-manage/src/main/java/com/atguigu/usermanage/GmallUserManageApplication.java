72e604e07d0e9cef9b651f2eb73c3231dbc59e21
daziran <343052690@qq.com>
2020-03-18 14:31:18 +0800

    mapper to git

--- gmall-user-manage/src/main/java/com/atguigu/usermanage/GmallUserManageApplication.java
+++ gmall-user-manage/src/main/java/com/atguigu/usermanage/GmallUserManageApplication.java
@@ -1,4 +1,4 @@
-package com.dazr.gmall.user;
+package com.atguigu.usermanage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ gmall-user-manage/src/test/java/com/atguigu/usermanage/GmallUserManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.usermanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
